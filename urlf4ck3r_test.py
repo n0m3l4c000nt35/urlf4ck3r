1809e38a10dcffb850acd60c76a298bbda4e6101
n0m3l4c000nt35 <estebanluiszarate1985@gmail.com>
2024-08-23 21:31:33 -0300

    Update

--- urlf4ck3r_test.py
+++ urlf4ck3r_test.py
@@ -4,7 +4,7 @@ import argparse
 import sys
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin, urlparse
+from urllib.parse import urljoin, urlparse, urlunparse
 from collections import defaultdict
 from pwn import *
 import pdb
@@ -17,10 +17,38 @@ GRAY = "\033[90m"
 PURPLE = "\033[95m"
 END_COLOR = "\033[0m"
 
+class Exiter:
+    def __init__(self):
+        self.state = False
+        signal.signal(signal.SIGINT, self.change_state)
+
+    def change_state(self, signum, frame):
+        print()
+        print(f"[{RED}!{END_COLOR}] Saliendo...")
+        print()
+        self.state = True
+
+    def exit(self):
+        return self.state
+
+flag = Exiter()
+
 def get_arguments():
     parser = argparse.ArgumentParser(prog="urlf4ck3r", description="Extraer las URL's del código fuente de una web", epilog="Creado por https://github.com/n0m3l4c000nt35")
     parser.add_argument("-u", "--url", type=str, dest="url", help="URL a escanear")
     return parser.parse_args(), parser
+
+def extract_subdomain(url):
+    netloc = urlparse(url).netloc.split(".")
+    if netloc[0] == "www":
+        subdomains = netloc[1:]
+    else:
+        subdomains = netloc
+    return ".".join(subdomains)
+
+def is_subdomain(base_url, url):
+    base_domain = urlparse(base_url).netloc
+    return url.endswith(base_domain) and url != base_domain
 
 def parse_url(url):
     parsed_url = urlparse(url)
@@ -47,23 +75,24 @@ if __name__ == "__main__":
     visited_urls = set()
     urls_to_visit = [base_url]
 
-    _, domain, _ = parse_url(base_url)
+    scheme, domain, _ = parse_url(base_url)
 
-    p = log.progress("")
+    #p = log.progress("")
     print()
     print(f"[{GREEN}DOMINIO{END_COLOR}]: {domain}")
     print()
 
-    while urls_to_visit:
+    while urls_to_visit and not flag.exit():
         url = urls_to_visit.pop(0)
         if url in visited_urls:
             continue
         visited_urls.add(url)
 
-        p.status(f"[{GREEN}CHECKING{END_COLOR}]: {url}")
+        #p.status(f"[{GREEN}CHECKING{END_COLOR}]: {url}")
+        print(f"[{GREEN}CHECKING{END_COLOR}]: {url}")
 
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
+        res = requests.get(url)
+        soup = BeautifulSoup(res.content, 'html.parser')
 
         for link in soup.find_all("a"):
             href = link.get("href")
@@ -71,14 +100,32 @@ if __name__ == "__main__":
             test_schemes = ["http", "https"]
             if href:
                 if not scheme:
-                    full_url = urljoin(base_url, path)
-                    all_urls["relative_urls"].add(full_url)
-                    if full_url not in visited_urls:
+                    full_url = urljoin(url, path)
+                    if full_url not in all_urls["relative_urls"]:
+                        all_urls["relative_urls"].add(full_url)
+                    if full_url not in visited_urls and full_url not in urls_to_visit:
                         urls_to_visit.append(full_url)
                 elif any(scheme in href for scheme in test_schemes):
-                    all_urls["absolute_urls"].add(href)
-                    if is_internal_url(base_url, href) and href not in visited_urls:
+                    if href not in all_urls["absolute_urls"]:
+                        all_urls["absolute_urls"].add(href)
+                    if is_internal_url(base_url, href) and href not in visited_urls and href not in urls_to_visit:
                         urls_to_visit.append(href)
+                    ext = extract_subdomain(href)
+                    if is_subdomain(base_url, ext) and ext not in all_urls["subdomains"]:
+                        subdomain = urlunparse((scheme, ext, "", "", "", ""))
+                        all_urls["subdomains"].add(subdomain)
+
+    #exit()
+    print()
+
+    print(f"[{GREEN}SUBDOMAINS{END_COLOR}]:")
+    if len(all_urls["subdomains"]) == 0:
+        print(f"[{RED}!{END_COLOR}] No se encontraron subdominios")
+    else:
+        for url in sorted(all_urls["subdomains"]):
+            print(url)
+
+    print()
 
     print(f"[{GREEN}ABSOLUTE URLS{END_COLOR}]:")
     if len(all_urls["absolute_urls"]) == 0:
@@ -105,5 +152,13 @@ if __name__ == "__main__":
     print()
 
     print(f"[{GREEN}URLS TO VISIT{END_COLOR}]:")
-    for url in sorted(urls_to_visit):
-        print(url)
+    if flag.exit():
+        print(f"[{RED}!{END_COLOR}] Quedaron por visitar {RED}{len(urls_to_visit)}{END_COLOR} URLs")
+        for url in sorted(urls_to_visit):
+            print(url)
+    elif len(urls_to_visit) == 0:
+        print(f"[{GREEN}+{END_COLOR}] Se chequearon todas las URLs posibles")
+    else:
+        print(f"[+] Quedaron por visitar {len(urls_to_visit)} URLs")
+        for url in sorted(urls_to_visit):
+            print(url)
